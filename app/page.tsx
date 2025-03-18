5990e99a4f8f25268107d7d0fa922d6eca334ea9
Parmar Rohit k. <86903998+ParmarRohitk@users.noreply.github.com>
2025-03-18 23:24:39 +0530

    update Home component to display a title instead of the component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import HomeComponent from "./components/HomeComponent";
+// import HomeComponent from "./components/HomeComponent";
 
 export default function Home() {
   return (
     <>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start" >
-        <HomeComponent />
+        <h1>Sticker Shop</h1>
+        {/* <HomeComponent /> */}
       </main>    </>
   );
 }
